461e8f9094cfca457b31999565826cb298cb9f57
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-10 22:29:04 +0100

    Implemented Carousel image clicking logic

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,12 +1,19 @@
 import { Component } from "react";
 
-class Carousel extends Component { 
+class Carousel extends Component {
   state = {
     active: 0,
   };
 
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+  // arrow function captures this context where it was written, e.g. Carousel class in this case
+  // regular function capture sthe context of where it was invoked
+  handleIndexClick = (e) => {
+    this.setState({ active: +e.target.dataset.index });
+    // target.dataset gets all values from DOM element attributes starting with "data-"
+    // all these attributes will return a string because everything in a DOM is a string
   };
 
   render() {
@@ -19,7 +26,10 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
+              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}

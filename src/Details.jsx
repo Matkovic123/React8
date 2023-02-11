eb497d123a2e29bf3c50fe5aa3907c7d6928f513
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-11 16:03:34 +0100

    Implemented modal with portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundarie";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   // "details" is an arbitrary caching keyvnjm,cf
   // if query cache is empty, call fetchPeet as last param
@@ -26,14 +29,28 @@ const Details = () => {
         <h2>
           {pet.animal} - {pet.animal} - {pet.city}, {pet.state}
         </h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {
+          showModal ? (
+            <Modal>
+              <div>
+                <h1> Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null /*rendering null means render nothing*/
+        }
       </div>
     </div>
   );
 };
 
-function DetailsErrorBoundary(props) { // forward props
+function DetailsErrorBoundary(props) {
+  // forward props
   return (
     <ErrorBoundary>
       <Details {...props} />

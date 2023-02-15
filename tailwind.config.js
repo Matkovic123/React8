7f1a82dc202016eb0e72af4ca464e9586ef8755f
Toni Matkovic <toni.matkovic0@gmail.com>
2023-02-16 00:19:01 +0100

    Added Tailwind and started replacing old CSS with it

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */  // @type comment allows autocompletion in VS code
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
